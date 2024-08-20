09cf2754b8d015a6c2fe4d69e0d81e807a150371
Daffa Akbar <daffaputradamar@gmail.com>
2024-08-20 11:02:43 +0700

    add theme

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -4,35 +4,8 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet"
 import { usePathname } from "next/navigation"
-
-function MoneyIcon(props: any) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg>
-    )
-}
-
-function MenuIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <line x1="4" x2="20" y1="12" y2="12" />
-            <line x1="4" x2="20" y1="6" y2="6" />
-            <line x1="4" x2="20" y1="18" y2="18" />
-        </svg>
-    )
-}
+import LogoIcon from "../icons/logo"
+import MenuIcon from "../icons/menu"
 
 export default function Navbar() {
 
@@ -66,7 +39,7 @@ export default function Navbar() {
             <div className="w-full max-w-7xl mx-auto px-4 py-2">
                 <div className="flex justify-between h-14 items-center">
                     <Link className="flex items-center" href="/">
-                        <MoneyIcon className="h-6 w-6" />
+                        <LogoIcon className="h-6 w-6 text-primary" />
                         <span className="sr-only">Daffa Inc</span>
                     </Link>
                     <nav className="hidden md:flex gap-5">
