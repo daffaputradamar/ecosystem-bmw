38d6b3305c92b4329bfa11552c8065d69347e8f7
Daffa Akbar <daffaputradamar@gmail.com>
2024-08-20 10:40:47 +0700

    add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@/components/ThemeProvider/theme-provider";
 import { ModeToggle } from "@/components/ModeToggle/mode-toggle";
 import Navbar from "@/components/Navbar/navbar";
 import Footer from "@/components/Footer/footer";
+import { Toaster } from "@/components/ui/sonner";
+import RootProviders from "@/providers/RootProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,18 +24,14 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`${inter.className} min-h-screen flex flex-col justify-between scrollbar`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <RootProviders>
           <Navbar />
-          <main>
+          <main className="flex-1 container">
             {children}
           </main>
-          <Footer/>
-        </ThemeProvider>
+          <Footer />
+          <Toaster richColors position="bottom-right" />
+        </RootProviders>
       </body>
     </html>
   );
